d06d7e1ebdd12d346362e788b05625b0e830f3a7
Baard A. Aspevik <thestigma@gmail.com>
2016-04-25 01:37:37 +0200

    New fancy upload using dropzone + post-redirect-get controllers

--- Product Browser/SmartCardManagementSystem/Controllers/HomeController.cs
+++ Product Browser/SmartCardManagementSystem/Controllers/HomeController.cs
@@ -36,7 +36,7 @@ namespace SmartCardManagementSystem.Controllers
             ViewData["tagID"] = tagID;
             ViewData["activePanel"] = "paneltitle_" + tagID;
 
-            return View("Overview",smartcardList);
+            return RedirectToAction("Overview", new { tagID = tagID });
         }
 
         //POST for changing name of smartcard
@@ -61,7 +61,7 @@ namespace SmartCardManagementSystem.Controllers
             ViewData["tagID"] = tagID;
             ViewData["activePanel"] = "paneltitle_" + tagID;
 
-            return View("Overview", smartcardList);
+            return RedirectToAction("Overview", new { tagID = tagID });
         }
 
 
@@ -113,13 +113,88 @@ namespace SmartCardManagementSystem.Controllers
             ViewData["tagID"] = tagID;
             ViewData["activePanel"] = "paneltitle_" + tagID;
 
+            return RedirectToAction("Overview", new { tagID = tagID });
+
+        }
+
+        [HttpPost]
+        public IActionResult OverviewUploadDataitemDZ(ICollection<IFormFile> files, int tagID)
+        {
+
+
+            var uploadfile = Request.Form.Files.FirstOrDefault();
+
+            System.Diagnostics.Debug.WriteLine("-------RUNNING UPLOAD----------");
+
+            var fileName = ContentDispositionHeaderValue.Parse(uploadfile.ContentDisposition).FileName.Trim('"');
+            var extension = fileName.Substring(Math.Max(0, fileName.Length - 3));
+
+            var readstream = uploadfile.OpenReadStream();
+            byte[] bytes;
+            bytes = new byte[readstream.Length];  //declare arraysize
+            readstream.Read(bytes, 0, bytes.Length); // read from stream to byte array
+
+            System.Diagnostics.Debug.WriteLine("-------Extension was----------");
+            System.Diagnostics.Debug.WriteLine(extension);
+
+            SmartCardDataItem item1;
+
+            //TODO PROPER INPUT CONTROL
+            //Create new dataitem
+            if (extension == "pdf")
+            {
+                System.Diagnostics.Debug.WriteLine("-------Creating document category----------");
+                item1 = new SmartCardDataItem(fileName, SmartCardDataItemCategory.Document, bytes);
+            }
+            else if (extension == "mp4")
+            {
+                System.Diagnostics.Debug.WriteLine("-------Creating video category----------");
+                item1 = new SmartCardDataItem(fileName, SmartCardDataItemCategory.Video, bytes);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("-------Creating image category----------");
+                item1 = new SmartCardDataItem(fileName, SmartCardDataItemCategory.Image, bytes);
+            }
+
+            //Add to smartcard based on tagID
+            ABBDataContext context = new ABBDataContext();
+            var smartcardList = context.SmartCards.OrderBy(a => a.TagId).ToList();
+            var cardToUpdate = smartcardList.Find(a => a.TagId == tagID);
+            cardToUpdate.DataItems.Add(item1);
+
+            context.SaveChanges();
+
+            ViewData["tagID"] = tagID;
+            ViewData["activePanel"] = "paneltitle_" + tagID;
+
             return View("Overview", smartcardList);
 
         }
 
-//General Overview GET
-[HttpGet]
-        public IActionResult Overview()
+        ////General Overview GET
+        //[HttpGet]
+        //public IActionResult Overview()
+        //{
+        //    System.Diagnostics.Debug.WriteLine("-------RUNNING GET----------");
+
+        //    ABBDataContext context = new ABBDataContext();
+        //    var smartcardList = context.SmartCards.OrderBy(a => a.TagId).ToList();
+        //    var x = context.SmartCardDataItems;
+
+        //    //var firstname = smartcardList[0].Name;
+
+        //    //ViewData["SmartcardList"] = smartcardList.Count;
+
+        //    ViewData["tagID"] = -1;
+        //    ViewData["activePanel"] = "paneltitle_" + -1;
+
+        //    return View(smartcardList);
+        //}
+
+        //General Overview GET, with tagID
+        [HttpGet]
+        public IActionResult Overview(int tagID)
         {
             System.Diagnostics.Debug.WriteLine("-------RUNNING GET----------");
 
@@ -131,7 +206,8 @@ namespace SmartCardManagementSystem.Controllers
 
             //ViewData["SmartcardList"] = smartcardList.Count;
 
-            ViewData["tagID"] = -1;
+            ViewData["tagID"] = tagID;
+            ViewData["activePanel"] = "paneltitle_" + tagID;
 
             return View(smartcardList);
         }
