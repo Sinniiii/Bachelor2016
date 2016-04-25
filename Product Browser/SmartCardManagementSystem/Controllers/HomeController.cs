4ec2be7a77830becebbb1b25855b0acade6a347c
Baard A. Aspevik <thestigma@gmail.com>
2016-04-25 18:41:35 +0200

    Fixed a routing bug for uploading
    Bugfix: overview did not remember position if first action was to upload
    files. now fixed via a workaround in javascript

--- Product Browser/SmartCardManagementSystem/Controllers/HomeController.cs
+++ Product Browser/SmartCardManagementSystem/Controllers/HomeController.cs
@@ -65,60 +65,63 @@ namespace SmartCardManagementSystem.Controllers
         }
 
 
+        //[HttpPost]
+        //public IActionResult OverviewUploadDataitem(IFormFile uploadfile, int tagID)
+        //{
+
+        //    System.Diagnostics.Debug.WriteLine("-------RUNNING UPLOAD----------");
+
+        //    var fileName = ContentDispositionHeaderValue.Parse(uploadfile.ContentDisposition).FileName.Trim('"');
+        //    var extension = fileName.Substring(Math.Max(0, fileName.Length - 3));
+
+        //    var readstream = uploadfile.OpenReadStream();
+        //    byte[] bytes;
+        //    bytes = new byte[readstream.Length];  //declare arraysize
+        //    readstream.Read(bytes, 0, bytes.Length); // read from stream to byte array
+
+        //    System.Diagnostics.Debug.WriteLine("-------Extension was----------");
+        //    System.Diagnostics.Debug.WriteLine(extension);
+
+        //    SmartCardDataItem item1;
+
+        //    //TODO PROPER INPUT CONTROL
+        //    //Create new dataitem
+        //    if (extension == "pdf")
+        //    {
+        //        System.Diagnostics.Debug.WriteLine("-------Creating document category----------");
+        //        item1 = new SmartCardDataItem(fileName, SmartCardDataItemCategory.Document, bytes);
+        //    }
+        //    else if (extension == "mp4")
+        //    {
+        //        System.Diagnostics.Debug.WriteLine("-------Creating video category----------");
+        //        item1 = new SmartCardDataItem(fileName, SmartCardDataItemCategory.Video, bytes);
+        //    }
+        //    else
+        //    {
+        //        System.Diagnostics.Debug.WriteLine("-------Creating image category----------");
+        //        item1 = new SmartCardDataItem(fileName, SmartCardDataItemCategory.Image, bytes);
+        //    }
+
+        //    //Add to smartcard based on tagID
+        //    ABBDataContext context = new ABBDataContext();
+        //    var smartcardList = context.SmartCards.OrderBy(a => a.TagId).ToList();
+        //    var cardToUpdate = smartcardList.Find(a => a.TagId == tagID);
+        //    cardToUpdate.DataItems.Add(item1);
+
+        //    context.SaveChanges();
+
+        //    System.Diagnostics.Debug.WriteLine("-------tagID was----------");
+        //    System.Diagnostics.Debug.WriteLine(tagID);
+
+        //    ViewData["tagID"] = tagID;
+        //    ViewData["activePanel"] = "paneltitle_" + tagID;
+
+        //    return RedirectToAction("Overview", new { tagID = tagID });
+
+        //}
+
         [HttpPost]
-        public IActionResult OverviewUploadDataitem(IFormFile uploadfile, int tagID)
-        {
-
-            System.Diagnostics.Debug.WriteLine("-------RUNNING UPLOAD----------");
-
-            var fileName = ContentDispositionHeaderValue.Parse(uploadfile.ContentDisposition).FileName.Trim('"');
-            var extension = fileName.Substring(Math.Max(0, fileName.Length - 3));
-
-            var readstream = uploadfile.OpenReadStream();
-            byte[] bytes;
-            bytes = new byte[readstream.Length];  //declare arraysize
-            readstream.Read(bytes, 0, bytes.Length); // read from stream to byte array
-
-            System.Diagnostics.Debug.WriteLine("-------Extension was----------");
-            System.Diagnostics.Debug.WriteLine(extension);
-
-            SmartCardDataItem item1;
-
-            //TODO PROPER INPUT CONTROL
-            //Create new dataitem
-            if (extension == "pdf")
-            {
-                System.Diagnostics.Debug.WriteLine("-------Creating document category----------");
-                item1 = new SmartCardDataItem(fileName, SmartCardDataItemCategory.Document, bytes);
-            }
-            else if (extension == "mp4")
-            {
-                System.Diagnostics.Debug.WriteLine("-------Creating video category----------");
-                item1 = new SmartCardDataItem(fileName, SmartCardDataItemCategory.Video, bytes);
-            }
-            else
-            {
-                System.Diagnostics.Debug.WriteLine("-------Creating image category----------");
-                item1 = new SmartCardDataItem(fileName, SmartCardDataItemCategory.Image, bytes);
-            }
-
-            //Add to smartcard based on tagID
-            ABBDataContext context = new ABBDataContext();
-            var smartcardList = context.SmartCards.OrderBy(a => a.TagId).ToList();
-            var cardToUpdate = smartcardList.Find(a => a.TagId == tagID);
-            cardToUpdate.DataItems.Add(item1);
-
-            context.SaveChanges();
-
-            ViewData["tagID"] = tagID;
-            ViewData["activePanel"] = "paneltitle_" + tagID;
-
-            return RedirectToAction("Overview", new { tagID = tagID });
-
-        }
-
-        [HttpPost]
-        public IActionResult OverviewUploadDataitemDZ(ICollection<IFormFile> files, int tagID)
+        public IActionResult OverviewUploadDataitemDZ(int tagID)
         {
 
 
@@ -165,9 +168,13 @@ namespace SmartCardManagementSystem.Controllers
 
             context.SaveChanges();
 
+            System.Diagnostics.Debug.WriteLine("-------tagID was----------");
+            System.Diagnostics.Debug.WriteLine(tagID);
+
             ViewData["tagID"] = tagID;
             ViewData["activePanel"] = "paneltitle_" + tagID;
 
+            //return RedirectToAction("Overview", new{ tagID = tagID });
             return View("Overview", smartcardList);
 
         }
@@ -209,7 +216,7 @@ namespace SmartCardManagementSystem.Controllers
             ViewData["tagID"] = tagID;
             ViewData["activePanel"] = "paneltitle_" + tagID;
 
-            return View(smartcardList);
+            return View("Overview",smartcardList);
         }
 
 
