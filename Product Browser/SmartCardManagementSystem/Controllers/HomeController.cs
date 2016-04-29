3df6ec32cbafdc401da34f101273ebac66f999ea
Baard A. Aspevik <thestigma@gmail.com>
2016-04-29 15:28:14 +0200

    dis fix?

--- Product Browser/SmartCardManagementSystem/Controllers/HomeController.cs
+++ Product Browser/SmartCardManagementSystem/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ using System.IO;
 using DatabaseModel.Model;
 using Microsoft.Net.Http.Headers;
 
+//asdadd
+
 namespace SmartCardManagementSystem.Controllers
 {
     public class HomeController : Controller
@@ -27,6 +29,7 @@ namespace SmartCardManagementSystem.Controllers
 
             ABBDataContext context = new ABBDataContext();
             var dataItemToRemove = context.SmartCardDataItems.First(a => a.Id == dataItemID);
+            context.Entry(dataItemToRemove).Reference(a => a.DataField).Load();
             context.SmartCardDataItems.Remove(dataItemToRemove);
             context.SaveChanges();
 
@@ -66,9 +69,18 @@ namespace SmartCardManagementSystem.Controllers
             System.Diagnostics.Debug.WriteLine("-------RUNNING POST - delete all dataitems----------");
 
             ABBDataContext context = new ABBDataContext();
-            var cardToUpdate = context.SmartCards.First(a => a.TagId == tagID);
+            //var cardToUpdate = context.SmartCards.First(a => a.TagId == tagID);
             //var cardToUpdate = context.SmartCards.Include(s => s.DataItems.Select(d => d.DataField)).FirstOrDefault(a => a.TagId == tagID);
-            cardToUpdate.DataItems.Clear();
+            //cardToUpdate.DataItems.Clear();
+
+            var list = context.SmartCardDataItems.Where(a => a.SmartCard.TagId == tagID);
+
+            foreach (var v in list)
+            {
+                context.Entry(v).Reference(a => a.DataField).Load();
+            }
+                
+            context.SmartCardDataItems.RemoveRange(list);
 
             context.SaveChanges();
 
@@ -85,8 +97,13 @@ namespace SmartCardManagementSystem.Controllers
 
             System.Diagnostics.Debug.WriteLine("-------RUNNING UPLOAD----------");
 
+            
+            
+
             var fileName = ContentDispositionHeaderValue.Parse(uploadfile.ContentDisposition).FileName.Trim('"');
-            var extension = fileName.Substring(Math.Max(0, fileName.Length - 3));
+            int lastPeriod = fileName.LastIndexOf('.');
+            string extension = fileName.Substring(lastPeriod + 1);
+            //var extension = fileName.Substring(Math.Max(0, fileName.Length - 3));
             
             System.Diagnostics.Debug.WriteLine("-------Extension was----------");
             System.Diagnostics.Debug.WriteLine(extension);
@@ -103,9 +120,14 @@ namespace SmartCardManagementSystem.Controllers
 
                 System.Diagnostics.Debug.WriteLine("-------Creating document category----------");
                 item1 = new SmartCardDataItem(fileName, SmartCardDataItemCategory.Document, bytes);
+
+                //Add to smartcard based on tagID
+                ABBDataContext context = new ABBDataContext();
+                var cardToUpdate = context.SmartCards.First(a => a.TagId == tagID);
+                cardToUpdate.DataItems.Add(item1);
+
+                context.SaveChanges();
             }
-<<<<<<< HEAD
-<<<<<<< HEAD
             //3gp 3g2 asx avi mp4 mpeg avi mov uvu tts wtv dvr-ms wm wmv wmx 
             else if (extension == "3gp"
                 || extension == "3g2"
@@ -119,22 +141,21 @@ namespace SmartCardManagementSystem.Controllers
                 || extension == "wmv"
                 || extension == "wmx"
                 )
-=======
-            else if (extension == "mp4")
->>>>>>> parent of 89e7041... support for more filetypes in upload
-=======
-            else if (extension == "mp4")
->>>>>>> parent of 89e7041... support for more filetypes in upload
             {
                 System.Diagnostics.Debug.WriteLine("-------Creating video category----------");
 
                 // Special stuff for making video items
                 item1 = new SmartCardDataItem(fileName);
 
+                //Add to smartcard based on tagID
+                ABBDataContext context = new ABBDataContext();
+                var cardToUpdate = context.SmartCards.First(a => a.TagId == tagID);
+                cardToUpdate.DataItems.Add(item1);
+
+                context.SaveChanges();
+
                 uploadfile.SaveAs(SmartCardDataItem.VIDEO_FOLDER + tagID + @"\" + fileName);
             }
-<<<<<<< HEAD
-<<<<<<< HEAD
             //bmp gif png jpg svg tiff dds wdp emf ico wmf
             else if (extension == "bmp"
                 || extension == "gif"
@@ -145,12 +166,6 @@ namespace SmartCardManagementSystem.Controllers
                 || extension == "ico"
                 || extension == "wmf"
                 )
-=======
-            else
->>>>>>> parent of 89e7041... support for more filetypes in upload
-=======
-            else
->>>>>>> parent of 89e7041... support for more filetypes in upload
             {
                 var readstream = uploadfile.OpenReadStream();
                 byte[] bytes = new byte[readstream.Length];  //declare arraysize
@@ -158,15 +173,18 @@ namespace SmartCardManagementSystem.Controllers
 
                 System.Diagnostics.Debug.WriteLine("-------Creating image category----------");
                 item1 = new SmartCardDataItem(fileName, SmartCardDataItemCategory.Image, bytes);
+
+                //Add to smartcard based on tagID
+                ABBDataContext context = new ABBDataContext();
+                var cardToUpdate = context.SmartCards.First(a => a.TagId == tagID);
+                cardToUpdate.DataItems.Add(item1);
+
+                context.SaveChanges();
             }
-
-            //Add to smartcard based on tagID
-
-            ABBDataContext context = new ABBDataContext();
-            var cardToUpdate = context.SmartCards.First(a => a.TagId == tagID);
-            cardToUpdate.DataItems.Add(item1);
-
-            context.SaveChanges();
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("-------INVALID FILE TYPE, DO NOTHING----------");
+            }
 
             ViewData["tagID"] = tagID;
             ViewData["activePanel"] = "paneltitle_" + tagID;
