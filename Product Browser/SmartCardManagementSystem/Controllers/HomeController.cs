a55aeee28c839b1151aab31ccba436bb467325f3
Baard A. Aspevik <thestigma@gmail.com>
2016-04-29 15:56:32 +0200

    fixed extension to new method

--- Product Browser/SmartCardManagementSystem/Controllers/HomeController.cs
+++ Product Browser/SmartCardManagementSystem/Controllers/HomeController.cs
@@ -203,7 +203,9 @@ namespace SmartCardManagementSystem.Controllers
             System.Diagnostics.Debug.WriteLine("-------RUNNING UPLOAD----------");
 
             var fileName = ContentDispositionHeaderValue.Parse(uploadfile.ContentDisposition).FileName.Trim('"');
-            var extension = fileName.Substring(Math.Max(0, fileName.Length - 3));
+            //var extension = fileName.Substring(Math.Max(0, fileName.Length - 3));
+            int lastPeriod = fileName.LastIndexOf('.');
+            string extension = fileName.Substring(lastPeriod + 1);
 
             System.Diagnostics.Debug.WriteLine("-------Extension was----------");
             System.Diagnostics.Debug.WriteLine(extension);
